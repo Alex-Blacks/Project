c0939beced1fc007e5668883c895b58bed1b19fd
Alex <alex@example.com>
2026-03-06 14:23:58 +0300

    Append function create task

--- GoWorkspace/Project/transport/grpc/server.go
+++ GoWorkspace/Project/transport/grpc/server.go
@@ -1,7 +1,29 @@
 package grpc
 
-import "Goworkspace/Project/api/proto"
+import (
+	"Goworkspace/Project/api/proto"
+	"Goworkspace/Project/domain"
+	"context"
+)
 
 type TaskServer struct {
 	proto.UnimplementedTaskServiceServer
+	service *domain.Service
+}
+
+func NewTaskServer(service *domain.Service) *TaskServer {
+	return &TaskServer{service: service}
+}
+
+func (s *TaskServer) CreateTask(ctx context.Context, req *proto.CreateTaskRequest) (*proto.Task, error) {
+	item, err := s.service.Create(ctx, req.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.Task{
+		Id:       int64(item.ID),
+		Name:     item.Name,
+		Complete: false,
+	}, nil
 }
